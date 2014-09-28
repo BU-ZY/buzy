530e2c88769d8b4fdf05cad15129b4960f9778a1
Deven <devend711@gmail.com>
2014-09-28 17:02:48 -0400

    Better map

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,16 +1,22 @@
 class StaticPagesController < ApplicationController
+	include ApplicationHelper
+
   def home
-  	view_context.update_scores
+  	update_scores
   	@average_score = Place.all.map{|place| place.score}.instance_eval{(reduce(:+)/size.to_f)}.round
   	@average_score_color = view_context.busyness_color(@average_score)
   end
 
   def map
+  	update_scores
   	file = File.open(File.join(Rails.root, 'app', 'assets', 'json', 'locations.json')).read
   	json = JSON.parse(file)
-  	@center_lat = json.find{|p| p['name'] == 'CENTER'}['lat']
-  	@center_long = json.find{|p| p['name'] == 'CENTER'}['long']
-  	@locations = Place.all.select{|p| p.location != ""}.map{|p| p.location}
+  	@center_lat = json.find{|p| p['name'] == 'CENTER'}['lat'] # find center of map
+  	@center_long = json.find{|p| p['name'] == 'CENTER'}['long'] 
+  	selection = Place.all.select{|p| p.location != "" && p.name!= "CENTER"} # find all the actual places
+  	@addresses = selection.map{|p| p.location.gsub(" ","").split(",")} # 2d array of [lat,long]
+  	@scores = selection.map{|p| p.score.to_s}
+  	@names = selection.map{|p| p.name}
   	render 'map'
   end
 end
