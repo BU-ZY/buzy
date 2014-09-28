a800de4bb1c24e14209a6f3bddae66342b827988
Deven <devend711@gmail.com>
2014-09-28 17:03:45 -0400

    Better map

--- db/migrate/20140928021425_make_places_from_json.rb
+++ db/migrate/20140928021425_make_places_from_json.rb
@@ -2,7 +2,6 @@ class MakePlacesFromJson < ActiveRecord::Migration
   def change
   	file = File.open(File.join(Rails.root, 'app', 'assets', 'json', 'locations.json')).read
   	json = JSON.parse(file)
-  	binding.pry
   	json.each do |p|
   		Place.create(name: p['name'], location:p['location']) unless p['name'] == "CENTER"
   	end
