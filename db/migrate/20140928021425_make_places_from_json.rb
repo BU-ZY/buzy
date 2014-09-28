1b8f289c84f32a9d3611efd1b1ee0121ae8adffb
Deven <devend711@gmail.com>
2014-09-28 17:34:30 -0400

    Seed data

--- db/migrate/20140928021425_make_places_from_json.rb
+++
@@ -1,9 +0,0 @@
-class MakePlacesFromJson < ActiveRecord::Migration
-  def change
-  	file = File.open(File.join(Rails.root, 'app', 'assets', 'json', 'locations.json')).read
-  	json = JSON.parse(file)
-  	json.each do |p|
-  		Place.create(name: p['name'], location:p['location']) unless p['name'] == "CENTER"
-  	end
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+# populate Places
+file = File.open(File.join(Rails.root, 'app', 'assets', 'json', 'locations.json')).read
+json = JSON.parse(file)
+json.each do |p|
+	Place.create(name: p['name'], location:p['location']) unless p['name'] == "CENTER"
+end
