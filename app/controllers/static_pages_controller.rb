eec95e6320922c22c7ae1f1ee0d6b28126b1961e
Alex Wong <alexwong@bu.edu>
2014-09-27 20:35:52 -0400

    Places now display on the home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,13 @@
 class StaticPagesController < ApplicationController
+  include ApplicationHelper
+  
   def home
   	view_context.update_scores
   	@average_score = Place.all.map{|place| place.score}.instance_eval{(reduce(:+)/size.to_f)}.round
   	# @average_score = Place.all.map{|place| place.score}.inject(:+)/Place.all.length
   	@average_score = 1
   	@average_score_color = view_context.busyness_color(@average_score)
+    @places_and_colors = places_and_colors
   end
 
   def map
@@ -17,4 +20,5 @@ class StaticPagesController < ApplicationController
   	raise "hi"
   	render 'map'
   end
+
 end
