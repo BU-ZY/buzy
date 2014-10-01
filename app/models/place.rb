1ca6685f9c77c113d10b9eea9eefe8ed30f563c0
Deven <devend711@gmail.com>
2014-10-01 00:52:27 -0400

    Schedule ingalls scrape

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,5 @@
+INGALLS_URL = "http://www.bu.edu/eng/current-students/ingalls/status/output.html"
+
 class Place < ActiveRecord::Base
 	has_many :votes
 	validates :name, presence: true, length: { maximum: 50 }
@@ -9,5 +11,21 @@ class Place < ActiveRecord::Base
 	def score(which_votes = nil) # default returns the weighted score for all recent votes
 		votes = which_votes ? which_votes : recent_votes.where(place_id: id)
 		weighted_score(votes)
+	end
+
+	def cast_vote_from_ingalls_page # method to be run by cron
+		begin
+			page = Nokogiri::HTML(open(PAGE_URL))
+			data = page.css("strong")
+			score = data[0].text.to_i
+			score = 100 if score > 100
+			ingalls_id = Place.find_by(name: 'Ingalls').id
+			5.times do # cast this vote 5 times
+				Vote.create(place_id: ingalls_id, score: score)
+			end
+			puts "Ingalls scrape successful!"
+		rescue: e # catch errors
+			puts "Ingalls score scraper error: #{e}"
+		end
 	end
 end
