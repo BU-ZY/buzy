1c5e96ecc85f5b4ad6851a16a5784502ae258262
Deven <devend711@gmail.com>
2014-10-15 00:48:44 -0400

    fix ordering of votes

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,6 +3,7 @@ class Vote < ActiveRecord::Base
 	belongs_to :user
 	validates :place_id, presence: true
 	validates :score, presence: true, numericality: { only_integer: true, less_than: 101}
+  default_scope order('created_at DESC')
 
 	def within?(x) # boolean t/f if the vote was cast in the last x minutes
 		raise "Expected an integer" unless x.is_a?(Integer)
