e9c12247c44b7295cacaff5abc4d0a405268b945
Deven <devend711@gmail.com>
2014-10-23 13:21:49 -0400

    make sure score is valid

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,6 +3,7 @@ class Vote < ActiveRecord::Base
 	belongs_to :user
 	validates :place_id, presence: true
 	validates :score, presence: true, numericality: { only_integer: true, less_than: 101}
+  validates :score, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 100 }
   default_scope order('created_at DESC')
 
 	def within?(x) # boolean t/f if the vote was cast in the last x minutes
